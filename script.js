e6349905acdb485d49648f8dd6ae8e182e2e4cd1
Yogesh More <yogesh@masterblocks.co.in>
2023-08-24 07:40:32 +0530

    Initial commit - I has basic layout for Excel Doc project

---
+++ script.js
@@ -0,0 +1,44 @@
+let rows = 100;
+let cols = 26;
+
+let addressColCont = document.querySelector(".address-col-cont");
+for(let i = 0; i < rows; i++) {
+  let addressCol = document.createElement("div");
+  addressCol.setAttribute("class", "address-col");
+  addressCol.innerText = i+1;
+  addressColCont.appendChild(addressCol);
+}
+
+let addressRowCont = document.querySelector(".address-row-cont");
+for(let i = 0; i < cols; i++) {
+  let addressRow = document.createElement("div");
+  addressRow.setAttribute("class", "address-row");
+
+  addressRow.innerText = String.fromCharCode(65+i);
+  addressRowCont.appendChild(addressRow);
+}
+
+
+let cellsCont = document.querySelector(".cells-cont");
+
+for(let i = 0; i < rows; i++){
+  let rowCont = document.createElement("div");
+  rowCont.setAttribute("class","row-cont");
+
+  for(let j = 0; j< cols; j++){
+    let cell = document.createElement("div");
+    cell.setAttribute("class","cell");
+    cell.setAttribute("contenteditable", true);
+    rowCont.appendChild(cell);
+    addListenerForAddressBarDisplay(cell, i, j);
+  }
+  cellsCont.appendChild(rowCont);
+}
+let addressBar = document.querySelector('.address-bar');
+function addListenerForAddressBarDisplay(cell, i, j){
+  cell.addEventListener("click", (e)=> {
+    let rowId = i+1;
+    let colId = String.fromCharCode(65+j);
+    addressBar.value =`${colId}${rowId}`;
+  })
+}
